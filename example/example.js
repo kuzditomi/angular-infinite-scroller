c75417ed1cb7b9e970fd93709a9f827261940470
Küzdi Tamás <kuzditomi@gmail.com>
2018-01-27 19:44:12 +0100

    add a bit more complex example

--- example/example.js
+++ example/example.js
@@ -4,26 +4,43 @@
     app.controller('ExampleController', [
         '$scope',
         function (scope) {
-            scope.szia = 'Hello';
 
+            // simple list
             let numbers = [];
             for (var i = 0; i < 100; i++) {
                 numbers.push(i);
             }
-
             scope.items = numbers;
 
-
+            // table
             let rows = [];
-
             for (var i = 0; i < 100; i++) {
                 rows.push({
                     id: i,
                     name: 'number ' + i
                 })
             }
-
             scope.rows = rows;
+
+            // bit complex
+            let stuffs = [];
+            let words = ['this', 'is', 'some', 'word', 'to', 'randomly', 'pick', 'three', 'with', 'some', 'super', 'complex', 'logic'];
+
+            for (var i = 0; i < 100; i++) {
+                let color = Math.random();
+                let name = "";
+                for (var j = 0; j < 3; j++) {
+                    name += words[Math.floor(Math.random() * words.length)] + " ";
+                }
+
+                stuffs.push({
+                    id: i,
+                    name: name,
+                    color: color < 0.66 ? (color < 0.33 ? 'green' : 'blue') : 'red'
+                });
+            }
+
+            scope.stuffs = stuffs;
         }
     ]);
 }())
